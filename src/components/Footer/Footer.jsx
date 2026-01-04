ec3e7927ac2103f3016ca91e9e3c36b8bb7038fa
Francis Kenne <harfleur20@gmail.com>
2026-01-04 15:25:00 +0100

    ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer-top">
         <div className="footer-top-left">
           <img src={footer_logo} alt="" />
-          <p>Je suis résilient et optiministe, mon objectif est <br /> de migrer mes compétences vers l'infra & le cloud.</p>
+          <p>Je suis résilient et optiministe, mon objectif est de migrer mes compétences vers l'infra & le cloud.</p>
         </div>
         <div className="footer-top-right">
           <div className="footer-email-input">
